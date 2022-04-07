8de4431a9257b186602621e715a80b585a2c436c
imkrrish <imkrrish227@gmail.com>
2022-04-08 00:12:31 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,35 +27,27 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fe77e85f5f5d06424d68.js"
+    "url": "webpack-runtime-9131a5d0cfccdb71794b.js"
   },
   {
     "url": "framework-d95c2cbb074a8fe18abc.js"
   },
   {
-    "url": "app-5260bafe761cc6458c47.js"
+    "url": "app-e39fb44d10ca3a9e3987.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fd38d556ef3ad1388088f3bcd18e0b97"
+    "revision": "fd741548d0a623eb13cca3102f923f0b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4b6bc86791a7ff8a1ce5.js"
-  },
-  {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "f2c002077289a7e1ac538802bc7f5314"
-  },
-  {
-    "url": "page-data/app-data.json",
-    "revision": "8f2eb85283a7cde49f7266a43a45b91c"
   },
   {
     "url": "polyfill-baaed29a46369ddb5def.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "717f6c41d348c844a3239755bbafbab8"
+    "revision": "95f2a7ad82eef8dedb444e62289a1e00"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -142,12 +134,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/imkrrish.github.io`), ``)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/imkrrish.github.io/app-5260bafe761cc6458c47.js`))) {
+  if (!resources || !(await caches.match(`/app-e39fb44d10ca3a9e3987.js`))) {
     return await fetch(event.request)
   }
 
@@ -160,7 +152,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/imkrrish.github.io/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
